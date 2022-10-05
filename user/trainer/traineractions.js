0d0dcb70bb52589b923edd6cc0dfd8e85950da63
Isaiah <isaiahc11@educbe.ca>
2022-10-04 20:14:52 -0600

    fix start

--- user/trainer/traineractions.js
+++ user/trainer/traineractions.js
@@ -1,18 +1,23 @@
-let a;
-let choiceTrue = null;
+let a, choiceTrue, chosenPokemon, nameResponse;
 
 const trainer = {
-    get name() {
-      return window.prompt('What is your name?')
-    },
-    choosePokemon: () => {
-      a = window.prompt('Choose your Pokemon (Pikachu)').toLowerCase();
-      switch (a) {
-        case 'pikachu':
-          choiceTrue = true;
-          break;
-        default:
-          choiceTrue = false;
-      };
-    },
-  };
+  get name() {
+    return window.prompt('What is your name?');
+  },
+  choosePokemon: () => {
+    a = window.prompt(`Hi there, ${trainer.name}, choose your Pokemon (Pikachu)`);
+    if (a === null) {
+      return;
+    };
+    switch (a.toLowerCase()) {
+      case 'pikachu':
+        choiceTrue = true;
+        break;
+      default:
+        choiceTrue = false;
+    };
+    if (choiceTrue) {
+      chosenPokemon = a.toLowerCase();
+    };
+  },
+};
