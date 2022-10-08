ad38b1ceb8260a224ab22a807de070e72a9c7ec4
xingadora <74115064+xingadora@users.noreply.github.com>
2022-10-08 16:24:41 -0700

    move file (delete later)

--- src/class/newpokemon.js
+++ src/class/newpokemon.js
@@ -1,17 +1,56 @@
 class pokemon {
-    constructor(hp, type, evoloution, retreatCost, weakness, weaknessMulti, resistance, resistanceMulti) {
+    constructor(name, hp, type, retreatCost, weakness, weaknessMulti, resistance, resistanceMulti) {
+        this.name = name;
         this.hp = hp;
         this.type = type;
-        this.evoloution = evoloution;
-        this.retreatCost = retreatCost;
+        this.retreatCost = 2;
         this.weakness = weakness;
-        this.weaknessMulti = weaknessMulti;
+        this.weaknessMulti = 2;
         this.resistance = resistance;
-        this.resistanceMulti = resistanceMulti;
+        this.resistanceMulti = 0.5;
     }
+
+    retreat() { };
+    attack() { };
+    isDead() {
+        if (this.hp <= 0) {
+            return true;
+        } else {
+            return false;
+        }
+    };
 };
 
+class enemyPokemon extends pokemon {
+    constructor(name, hp, type, weakness, resistance) {
+        super();
+        this.name = name;
+        this.hp = hp;
+        this.type = type;
+        this.weakness = weakness;
+        this.resistance = resistance;
+    }
+}
 
 class userPokemon extends pokemon {
+    constructor(name, hp, type, weakness, resistance) {
+        super();
+        this.name = name;
+        this.hp = hp;
+        this.type = type;
+        this.weakness = weakness;
+        this.resistance = resistance;
+    }
 
+    attack(attackId) {
+        if (attackId === 'gnaw') {
+            // return attackGnaw();
+        } else if (attackId === 'thunderJolt') {
+            return attackThunderJolt();
+        }
+    };
 };
+
+const pikachu = new userPokemon('Pikachu', 60, 'Electric', 'Strength', 'Metal');
+const enemyPikachu = new enemyPokemon('Pikachu', 60, 'Electric', 'Strength', 'Metal');
+// console.log(pikachu.attack('gnaw'))
