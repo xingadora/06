1b414076fb28a6b11d2444223071d3df0af81b8f
Isaiah <isaiahc11@educbe.ca>
2022-10-27 22:34:00 -0600

    add text renderer

--- src/function/showstart.js
+++ src/function/showstart.js
@@ -1,3 +1,5 @@
+import { typewriter } from "./textrenderer.js";
+
 function showNext() {
   nextId = document.querySelectorAll(".hidden");
   nextId[0].style.display = "block";
@@ -12,4 +14,28 @@ function showInvalid() {
 function hideInvalid() {
   invalidId = document.querySelectorAll(".invalid");
   invalidId[0].style.display = "none";
+}
+
+document.addEventListener("keydown", hideStart);
+let overlay = document.getElementById("overlay");
+
+function hideStart() {
+  let pressEnter = document.getElementById("press-enter");
+  document.removeEventListener("keydown", hideStart);
+  pressEnter.classList.remove("press-enter");
+  pressEnter.classList.add("pressed-enter");
+  overlay.classList.add("overlay");
+  overlay.addEventListener("animationend", showStart);
+}
+
+function showStart() {
+  overlay.removeEventListener("animationend", showStart);
+  document.body.classList.remove("container")
+  document.body.style.backgroundImage = "url('/src/img/start_background.jpg')";
+  overlay.classList.remove("overlay");
+  void overlay.offsetWidth;
+  overlay.classList.add("overlay");
+  overlay.style.animationDirection = "reverse";
+  overlay.style.animationFillMode = "none";
+  typewriter();
 }
